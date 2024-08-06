38475ed73cbcb37a60ee1e8f16f0acc0dc0eaf2b
Rubén Y <rubenmeju@outlook.es>
2024-08-06 21:30:05 +0200

    añadir animaciones movimiento player

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -13,6 +13,52 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     // El jugador no puede salir del mundo
     this.setCollideWorldBounds(true);
+
+    // Crear las animaciones
+    this.createAnimations(scene);
+  }
+
+  createAnimations(scene) {
+    // Animación para mover a la izquierda
+    scene.anims.create({
+      key: "left",
+      frames: scene.anims.generateFrameNumbers("player", { start: 0, end: 2 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    // Animación para mover a la derecha (segunda fila)
+    scene.anims.create({
+      key: "right",
+      frames: scene.anims.generateFrameNumbers("player", {
+        start: 14,
+        end: 16,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    // Animación para mover hacia arriba (tercera fila)
+    scene.anims.create({
+      key: "up",
+      frames: scene.anims.generateFrameNumbers("player", {
+        start: 17,
+        end: 19,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    // Animación para mover hacia abajo (cuarta fila)
+    scene.anims.create({
+      key: "down",
+      frames: scene.anims.generateFrameNumbers("player", {
+        start: 3,
+        end: 5,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
   }
 
   update(cursors) {
@@ -21,18 +67,32 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     // Detectar si las teclas de dirección están siendo presionadas
     if (cursors.left.isDown) {
       this.setVelocityX(-velocidad); // Mover a la izquierda con velocidad
+      this.anims.play("left", true); // Reproducir animación hacia la izquierda
     } else if (cursors.right.isDown) {
       this.setVelocityX(velocidad); // Mover a la derecha con velocidad
+      this.anims.play("right", true); // Reproducir animación hacia la derecha
     } else {
       this.setVelocityX(0); // Detener movimiento horizontal si no se presiona ninguna tecla
     }
 
     if (cursors.up.isDown) {
       this.setVelocityY(-velocidad); // Mover hacia arriba con velocidad
+      this.anims.play("up", true); // Reproducir animación hacia arriba
     } else if (cursors.down.isDown) {
       this.setVelocityY(velocidad); // Mover hacia abajo con velocidad
+      this.anims.play("down", true); // Reproducir animación hacia abajo
     } else {
       this.setVelocityY(0); // Detener movimiento vertical si no se presiona ninguna tecla
+    }
+
+    // Detener la animación si no se presiona ninguna tecla
+    if (
+      !cursors.left.isDown &&
+      !cursors.right.isDown &&
+      !cursors.up.isDown &&
+      !cursors.down.isDown
+    ) {
+      this.anims.stop();
     }
   }
 }
